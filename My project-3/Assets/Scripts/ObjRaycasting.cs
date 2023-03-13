b9187f0eb6e021900ee352c3692b77da64eb62e2
Osama-Mushtaq <108954241+Osama-Mushtaq@users.noreply.github.com>
2023-03-13 12:29:42 +0500

    raycasting1

---
+++ My project-3/Assets/Scripts/ObjRaycasting.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjRaycasting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 100))
+        {
+            Renderer renderer = hit.collider.GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                // change the color of the object hit to red
+                renderer.material.color = Color.blue;
+            }
+        }
+    }
+}
