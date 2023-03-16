63bb3d201e2b2d21de8c58f5a085eb430843d51b
Osama-Mushtaq <108954241+Osama-Mushtaq@users.noreply.github.com>
2023-03-16 22:45:24 +0500

    doing crosshair stuff on ObjRaycasting.cs

--- My project-3/Assets/Scripts/ObjRaycasting.cs
+++ My project-3/Assets/Scripts/ObjRaycasting.cs
@@ -5,7 +5,13 @@ using UnityEngine.UI;
 
 public class ObjRaycasting : MonoBehaviour
 {
+    //!declarations for changing crosshair color
+    public Canvas crshr;
+    private Color32 color = new Color(0.5f, 1f, 0.5f, 1f);
+    //! declarations end for changing crosshair color
+    public float waitTime = 4f;
 
+    private bool isWaiting = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,13 +22,43 @@ public class ObjRaycasting : MonoBehaviour
     void Update()
     {
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, 100))
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 100) && Input.GetMouseButtonDown(0))
         {
             Renderer renderer = hit.collider.GetComponent<Renderer>();
             if (renderer.name == "Sphere")
             {
-
+                StartCoroutine(WaitAndChangeColor());
             }
         }
+    }
+    IEnumerator WaitAndChangeColor()
+    {
+        // Check if already waiting
+        if (isWaiting)
+        {
+            yield break;
+        }
+
+        // Set waiting flag
+        isWaiting = true;
+
+        // Wait for the specified time
+        yield return new WaitForSeconds(waitTime);
+
+        // Change the color of the crosshair
+        //!Code for changing crosshair color
+        Transform objectTransform = crshr.transform.Find("my_crosshair");
+        Graphic objectGraphic = objectTransform.GetComponentInChildren<Graphic>();
+        objectGraphic.color = color;
+
+        Image[] objectImages = objectTransform.GetComponentsInChildren<Image>();
+        foreach (Image image in objectImages)
+        {
+            image.color = color;
+        }
+        //!code end dor changing crosshair color
+
+        // Reset waiting flag
+        isWaiting = false;
     }
 }
