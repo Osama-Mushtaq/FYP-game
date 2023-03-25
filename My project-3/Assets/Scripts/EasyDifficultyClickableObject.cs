d3652b10a9df32ab66558ac6dba5937b4406edbe
Osama-Mushtaq <108954241+Osama-Mushtaq@users.noreply.github.com>
2023-03-25 17:31:28 +0500

    Update EasyDifficultyClickableObject.cs

--- My project-3/Assets/Scripts/EasyDifficultyClickableObject.cs
+++ My project-3/Assets/Scripts/EasyDifficultyClickableObject.cs
@@ -31,44 +31,4 @@ public class EasyDifficultyClickableObject : MonoBehaviour
             transform.position = newPosition;
         }
     }
-    //*using update
-    // void Update()
-    // {
-    //     RaycastHit hit;
-    //     if (Input.GetMouseButtonDown(0) && Physics.Raycast(transform.position, transform.forward, out hit, 100))
-    //     {
-    //         Renderer renderer = hit.collider.GetComponent<Renderer>();
-    //         if (renderer.name == "Sphere")
-    //         {
-    //             Vector3 size = transform.lossyScale;
-    //             Vector3 pos = transform.position;
-    //             Vector3 newPosition = transform.position;
-    //             // newPosition.x = Random.Range(-spawnRange.x, spawnRange.x);
-    //             // newPosition.y = Random.Range(size.y, spawnRange.y);
-    //             newPosition.x = pos.x;
-    //             newPosition.y = pos.y;
-    //             newPosition.z = Random.Range(spawnRange.z, (float)2 * spawnRange.z);
-    //             transform.position = newPosition;
-    //         }
-    //     }
-    // }
-
 }
-
-/**
-        //!declarations for changing crosshair color
-        public Canvas crshr;
-        private Color32 color = new Color(0.5f, 1f, 0.5f, 1f);
-        //! declarations end for changing crosshair color
-        //!Code for changing crosshair color
-        Transform objectTransform = crshr.transform.Find("my_crosshair");
-        Graphic objectGraphic = objectTransform.GetComponentInChildren<Graphic>();
-        objectGraphic.color = color;
-
-        Image[] objectImages = objectTransform.GetComponentsInChildren<Image>();
-        foreach (Image image in objectImages)
-        {
-            image.color = color;
-        }
-        //!code end dor changing crosshair color
-*/
