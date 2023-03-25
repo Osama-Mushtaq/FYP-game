12c6c1f7f1177210bc0263f6d1f4e27bc7f49885
Osama-Mushtaq <108954241+Osama-Mushtaq@users.noreply.github.com>
2023-03-25 17:15:52 +0500

    v

--- My project-3/Assets/Scripts/ObjRaycasting.cs
+++ My project-3/Assets/Scripts/ObjRaycasting.cs
@@ -6,23 +6,23 @@ using UnityEngine.UI;
 public class ObjRaycasting : MonoBehaviour
 {
     public Canvas crshr;
-    public GameObject objbody;
     private Color32 originalColor = new Color32(255, 255, 255, 255);
     private Color32 targetColor = new Color32(127, 255, 127, 255);
-    public float waitTime = 4f;
+    private float waitTime = 2f;
     private Vector3 spawnRange = new Vector3(2, 4, 5);
 
     private bool isWaiting = false;
     private bool isTargetObject = false;
+    private bool canHit = false;
     private Coroutine colorCoroutine;
 
     void Update()
     {
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, 100))
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
         {
-            Renderer renderer = hit.collider.GetComponent<Renderer>();
-            if (renderer.name == "Sphere")
+
+            if (hit.collider.gameObject.name == "Sphere")
             {
                 isTargetObject = true;
                 if (!isWaiting)
@@ -36,7 +36,7 @@ public class ObjRaycasting : MonoBehaviour
                 isWaiting = false;
                 if (colorCoroutine != null)
                 {
-                    StopCoroutine(colorCoroutine);
+                    StopCoroutine(WaitAndChangeColor());
                 }
                 ResetColor();
             }
@@ -45,10 +45,6 @@ public class ObjRaycasting : MonoBehaviour
     IEnumerator WaitAndChangeColor()
     {
         isWaiting = true;
-        if (Input.GetMouseButtonDown(0) && isWaiting)
-        {
-            //do nothing
-        }
         yield return new WaitForSeconds(waitTime);
         if (isTargetObject)
         {
@@ -63,17 +59,7 @@ public class ObjRaycasting : MonoBehaviour
         }
         //TODO: can add the new code here of CLICKING
         isWaiting = false;
-
-        if (Input.GetMouseButtonDown(0) && !isWaiting)
-        {
-            Vector3 size = objbody.transform.lossyScale;
-            Vector3 pos = objbody.transform.position;
-            Vector3 newPosition = objbody.transform.position;
-            newPosition.x = pos.x;
-            newPosition.y = pos.y;
-            newPosition.z = Random.Range(spawnRange.z, (float)2 * spawnRange.z);
-            transform.position = newPosition;
-        }
+        canHit = true;
     }
     void ResetColor()
     {
